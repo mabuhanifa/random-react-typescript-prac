f56b5b845f90773110d194eff0bbb5ecbf30f0d2
shourovahmd <moh.abuhanifa@gmail.com>
2022-11-30 03:53:02 +0600

    useReducer

--- src/components/NewReducer.tsx
+++ src/components/NewReducer.tsx
@@ -1,35 +1,7 @@
-import { useReducer, useState } from 'react';
-type StateType = {
-    id: number;
-    todo: string;
-}
-type Action =
-    { type: "ADD_TODO", todo: string } |
-    { type: "REMOVE_TODO", id: number };
+import { useState } from 'react';
 
-const initialState: StateType[] = [{
-    id: 0,
-    todo: "first"
-}]
-
-const reducer = (state: StateType[], action: Action) => {
-    switch (action.type) {
-        case "ADD_TODO":
-            return [
-                ...state,
-                {
-                    id: state.length + 1,
-                    todo: action.todo
-                },
-            ];
-        case "REMOVE_TODO":
-            return state.filter(({ id }) => id !== action.id);
-        default:
-            throw new Error();
-    }
-}
 export default function NewReducer() {
-    const [state, dispatch] = useReducer(reducer, initialState);
+
     const [text, setText] = useState("");
     return (
         <div>
--- src/context/Context.tsx
+++ src/context/Context.tsx
@@ -1,11 +1,40 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useReducer } from "react";
 type ContextChild = {
     children: ReactNode
 }
+type StateType = {
+    id: number;
+    todo: string;
+}
+type Action =
+    { type: "ADD_TODO", todo: string } |
+    { type: "REMOVE_TODO", id: number };
 
+const initialState: StateType[] = [{
+    id: 0,
+    todo: "first"
+}]
+
+const reducer = (state: StateType[], action: Action) => {
+    switch (action.type) {
+        case "ADD_TODO":
+            return [
+                ...state,
+                {
+                    id: state.length + 1,
+                    todo: action.todo
+                },
+            ];
+        case "REMOVE_TODO":
+            return state.filter(({ id }) => id !== action.id);
+        default:
+            throw new Error();
+    }
+}
 export default function Context({ children }: ContextChild) {
     const TodoContext = createContext({});
+    const [state, dispatch] = useReducer(reducer, initialState);
     return (
-        <TodoContext.Provider value={{}}>Context</TodoContext.Provider>
+        <TodoContext.Provider value={{}}>{children}</TodoContext.Provider>
     )
 }
