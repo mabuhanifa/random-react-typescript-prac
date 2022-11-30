685dbe31d8b412eef12c3633532c743e8807ba1d
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-01 02:04:22 +0600

    data fetched

--- src/components/NewReducer.tsx
+++ src/components/NewReducer.tsx
@@ -1,12 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useShop } from '../context/ShopContext';
 
 export default function NewReducer() {
-    const { state, dispatch } = useShop();
-    console.log(state, dispatch);
-    const [text, setText] = useState("");
+    const { state: { products, cart }, dispatch } = useShop();
+
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await fetch("https://dummyjson.com/products");
+            const _data = await res.json();
+            dispatch({
+                type: 'ADD_DATA',
+                payload: _data.products
+            })
+        }
+        fetchData();
+    }, [])
+
     return (
         <div>
+            <h1>Products</h1>
+            {
+                products && products.map((p) =>
+                    <div>
+                        {p.id}
+                    </div>
+                )
+            }
 
         </div>
     )
