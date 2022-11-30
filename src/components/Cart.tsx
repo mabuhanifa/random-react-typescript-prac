3df1f7ee5967fc4010919d2b7ac4dfd0cbba7b9f
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-01 05:38:53 +0600

    updating cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,6 +1,24 @@
+import { useShop } from "../context/ShopContext";
 
 export default function Cart() {
+    const { state: { cart }, dispatch } = useShop();
+    console.log(cart);
     return (
-        <div>Cart</div>
+        <div>
+            {
+                cart && cart.map((c) =>
+                    <div key={c.id}>
+                        {c.title}{c.quantity}
+                    </div>
+
+
+                )
+            }
+            <button onClick={() => dispatch({
+                type: "CHANGE_CART_QTY",
+                payload: 
+            })}>+</button>
+            <button onClick={() => dispatch()}>-</button>
+        </div>
     )
 }
