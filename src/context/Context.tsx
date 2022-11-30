353ae99cd35070c91ec4fd8418b0ef1d3f1c71bd
shourovahmd <moh.abuhanifa@gmail.com>
2022-11-30 23:33:57 +0600

    shop context

--- src/context/Context.tsx
+++ src/context/Context.tsx
@@ -1,28 +1,42 @@
-import { createContext, useContext, useReducer } from "react";
-import { Action, ContextChild, MainState, StateType } from "../types/types";
+import { createContext, ReactNode, useContext, useReducer } from "react";
 
+export type ContextChild = {
+    children: ReactNode
+}
+export type StateType = {
+    id: number;
+    todo: string;
+}
+export type Action =
+    { type: "ADD_TODO", todo: string } |
+    { type: "REMOVE_TODO", id: number };
+
+export type MainState = {
+    state: StateType[];
+    dispatch: React.Dispatch<Action>
+}
 const initialState: StateType[] = [{
     id: 0,
     todo: "first"
 }]
 
 const reducer = (state: StateType[], action: Action) => {
-    switch (action.type) {
-        case "ADD_TODO":
-            return [
-                ...state,
-                {
-                    id: state.length + 1,
-                    todo: action.todo
-                },
-            ];
-        case "REMOVE_TODO":
-            return state.filter(({ id }) => id !== action.id);
-        default:
-            throw new Error();
-    }
+    // switch (action.type) {
+    //     case "ADD_TODO":
+    //         return [
+    //             ...state,
+    //             {
+    //                 id: state.length + 1,
+    //                 todo: action.todo
+    //             },
+    //         ];
+    //     case "REMOVE_TODO":
+    //         return state.filter(({ id }) => id !== action.id);
+    //     default:
+    //         throw new Error();
+    // }
 }
-const TodoContext = createContext({} as MainState);
+const ShopContext = createContext({} as MainState);
 export default function Context({ children }: ContextChild) {
 
     const [state, dispatch] = useReducer(reducer, initialState);
