36df1a9212c82b6f11bec9c7e10693df1530370e
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-01 18:06:04 +0600

    total price

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -12,20 +12,35 @@ export default function Cart() {
         })
         console.log(id, quantity)
     }
+    const totalPrice = cart.reduce((a, c) => a + ((c.price ? c.price : 0) * (c.quantity ? c.quantity : 0)), 0)
     return (
-        <div>
-            {
-                cart && cart.map((c) =>
+        <>
+            <h1>Cart</h1>
+            <h2>Total Items: {cart.length}</h2>
+            <h2>Total Price: {totalPrice}</h2>
 
-                    <div key={c.id}>
-                        {c.title}
-                        {c.quantity}
-                        <button className="btn__add" onClick={() => changeQuantity(c.id, c.quantity ? c.quantity + 1 : 1)}>+</button>
-                        <button className="btn__remove" onClick={() => changeQuantity(c.id, c.quantity ? c.quantity - 1 : 1)}>-</button>
-                    </div>
+            <div className="cart">
 
-                )
-            }
-        </div >
+
+                {
+                    cart && cart.map((c) =>
+
+                        <div key={c.id} className="cart_body">
+                            <h4>{c.title}</h4>
+                            <p>Quantity: {c.quantity}</p>
+                            <p>Price: {c.price}</p>
+
+                            <div className="btn_div">
+                                <button className="btn__add"
+                                    onClick={() => changeQuantity(c.id, c.quantity ? c.quantity + 1 : 1)}>+</button>
+                                <button className="btn__remove"
+                                    onClick={() => changeQuantity(c.id, c.quantity ? c.quantity - 1 : 1)}>-</button>
+                            </div>
+                        </div>
+
+                    )
+                }
+            </div >
+        </>
     )
 }
