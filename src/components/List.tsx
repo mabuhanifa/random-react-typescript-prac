529e7778bc3e25d728da24f12f8db8267bf96f16
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-05 00:10:14 +0600

    onClick

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,8 +1,10 @@
-const List: React.FunctionComponent<{ items: string[]; onClick?: (item: string) => void }> = ({ items }) => (
+const List: React.FunctionComponent<{ items: string[]; onClick?: (item: string) => void }> = ({ items, onClick }) => (
     <ul>
         {
             items.map((item, index) => (
-                <li key={index}>{item}</li>
+                <li key={index} onClick={() => onClick?.(item)}>
+                    {item}
+                </li>
             ))
         }
     </ul>
