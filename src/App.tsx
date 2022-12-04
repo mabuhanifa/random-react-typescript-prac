d7b3ba99f5d603f9deff98f0372f8e0c5467888f
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-04 23:21:38 +0600

    no-bs-ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import './App.css';
-import NewReducer from './components/NewReducer';
-import ShopContext from './context/ShopContext';
-
-
 
 function App() {
   return (
-    <ShopContext>
-      {/* <UseReducer /> */}
-      <NewReducer />
-    </ShopContext>)
+    <div>
+
+    </div>
+  )
 }
 
 export default App
