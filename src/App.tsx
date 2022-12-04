13fcad4e8a7c82ce37d220147c9c1994f2c6ecb8
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-04 23:28:27 +0600

    box

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css';
+import Box from './components/Box';
 import Heading from './components/Heading';
 
 function App() {
   return (
     <div>
       <Heading title={"Introduction"} />
+      <Box>
+        Hello There
+      </Box>
     </div>
   )
 }
