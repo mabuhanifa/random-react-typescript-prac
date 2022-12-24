3ff373d52724ed40fa0c44534bbbbd6bd26497e3
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-24 22:32:25 +0600

    state fetched from context

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useCallback } from 'react';
 import './App.css';
 import Box from './components/Box';
 import Heading from './components/Heading';
+import { useNew } from './components/NewContext';
 import Head from './components/ReactFunctionComponent';
 import UL from './typeComponent/GenComp';
 
@@ -12,6 +13,8 @@ function App() {
   const onListClick = useCallback((item: string) => {
     alert(item);
   }, []);
+  const { state } = useNew();
+  console.log(state)
   return (
     <div>
       <Heading title={"Introduction"} />
